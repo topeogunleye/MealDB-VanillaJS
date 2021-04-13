a36640a78070cb3385d52db265b15817d718cff4
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-04-13 10:29:54 +0100

    Adds CSS for menu items

--- public/script.js
+++ public/script.js
@@ -27,9 +27,9 @@ function searchMeal(e) {
           resultHeading.innerHTML = `<p>Could not find ${term.toUpperCase()}. Try again!</p>`
         } else {
           mealsEL.innerHTML = data.meals.map(meal =>   `
-            <div class="meal">
-              <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
-              <div class="meal-info" deal-mealID="${meal.idMeal}">
+            <div class="meal cursor-pointer relative h-44 w-44 text-center">
+              <img class="border-img w-full h-full" src="${meal.strMealThumb}" alt="${meal.strMeal} >
+              <div class="meal-info absolute top-0 left-0 h-full w-full bg-black bg-opacity-70 flex justify-center items-center transition-opacity duration-200 ease-in opacity opacity-0 hover:opacity-100" deal-mealID="${meal.idMeal}"> 
                 <h3>${meal.strMeal}</h3>
               </div>
             </div>
